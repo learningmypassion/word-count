fffb221229b4741a69e1d0aaff0f7ed0fb38d1ad
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-06-23 10:56:13 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer"
+
+const answers:{
+    sentence:string
+} = await inquirer.prompt([{
+
+    name: "sentence",
+    type:"input",
+    message:"enter your sentence to count the word:"
+}
+
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(`your word count is ${words.length}`);
